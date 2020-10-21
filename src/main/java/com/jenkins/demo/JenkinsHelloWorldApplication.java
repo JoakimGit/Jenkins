d80174b4552dd3b9e28cdef9c6e9d6429e3047ce
JoakimGit <jokke34@gmail.com>
2020-10-21 15:50:36 +0200

    Initial commit

--- src/main/java/com/jenkins/demo/JenkinsHelloWorldApplication.java
+++ src/main/java/com/jenkins/demo/JenkinsHelloWorldApplication.java
@@ -9,6 +9,7 @@ public class JenkinsHelloWorldApplication {
     public static void main(String[] args) {
         SpringApplication.run(JenkinsHelloWorldApplication.class, args);
         System.out.println("Hello World");
+        System.out.println("We meet again");
     }
 
 }
